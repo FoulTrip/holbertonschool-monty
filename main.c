17e27b735d8b7703896cd63479ff7fda87ec0839
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-09-07 18:05:25 -0500

    act003

--- main.c
+++ main.c
@@ -1,133 +1,175 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-typedef struct stack_t
+#include "monty.h"
+/**
+ * main - Monty interpreter main function
+ * @argc: Argument count
+ * @argv: Argument values
+ * Return: 0 on success, 1 on failure
+ */
+int main(int argc, char *argv[])
 {
-        int n;
-        struct stack_t *prev;
-        struct stack_t *next;
-} stack_t;
+	char *opcode, *value, *line = NULL;
+	size_t line_number = 0, len = 0;
+	FILE *file;
+	int i;
 
-typedef struct instructions
-{
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number, char *arg);
-} instructions;
-
-void push(stack_t **stack, unsigned int line_number, char *arg)
-{
-	int value = 0;
-	stack_t *new_node = malloc(sizeof(stack_t));
-
-	if (!new_node)
+	if (argc != 2)
 	{
-		printf("Error al asignar memoria\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-
-	if (!arg || (value = atoi(arg)) == 0)
+	file = fopen(argv[1], "r");
+	if (file == NULL)
 	{
-		printf("Error en la linea %u: Solo numeros enteros\n", line_number);
-		free(new_node);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
+	while (getline(&line, &len, file) != -1)
+	{
+		line_number++;
+		opcode = strtok(line, " \t$\n");
+		if (opcode == NULL || opcode[0] == '#')
+			continue;
+		if (strcmp(opcode, "push") == 0)
+		{
+			value = strtok(NULL, " $\n");
+			if (value == NULL)
+			{
+				fprintf(stderr, "L%lu: usage: push integer\n", line_number);
+				fclose(file);
+				free(line);
+				exit(EXIT_FAILURE);
+			}
+			for (i = 0; value[i] != '\0'; i++)
+			{
+				if (value[i] != '-' && !isdigit(value[i]))
+				{
+					fprintf(stderr, "L%lu: usage: push integer\n", line_number);
+					fclose(file);
+					free(line);
+					exit(EXIT_FAILURE);
+				}
+			}
+			push(&stack, atoi(value));
+		}
+		else if (strcmp(opcode, "pall") == 0)
+		{
+			pall(&stack, line_number);
+		}
+		else if (strcmp(opcode, "pint") == 0)
+		{
+			if (stack == NULL)
+			{
+				fprintf(stderr, "L%lu: can't pint, stack empty\n", line_number);
+				fclose(file);
+				exit(EXIT_FAILURE);
+			}
+			pint(&stack, line_number);
+		}
+		else if (strcmp(opcode, "pop") == 0)
+		{
+			if (stack == NULL)
+			{
+				fprintf(stderr, "L%lu: can't pop an empty stack\n", line_number);
+				fclose(file);
+				exit(EXIT_FAILURE);
+			}
 
+			 pop(&stack, line_number);
+		}
+		else
+		{
+			fprintf(stderr, "L%lu: unknown instruction %s\n", line_number, opcode);
+			fclose(file);
+			free(line);
+			free_dlistint(stack);
+			exit(EXIT_FAILURE);
+		}
+		free(line);
+		line = NULL;
+	}
+	free_dlistint(stack);
+	free(line);
+	fclose(file);
+	return (0);
+}
+/**
+ * push - Pushes an element onto the stack
+ * @stack: Pointer to the stack
+ * @value: Value to be pushed onto the stack
+ */
+void push(stack_t **stack, int value)
+{
+	stack_t *new_node;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 	new_node->n = value;
 	new_node->prev = NULL;
 	new_node->next = *stack;
-
-	if (*stack)
-	{
+	if (*stack != NULL)
 		(*stack)->prev = new_node;
-	}
-
 	*stack = new_node;
 }
-
-void pall(stack_t **stack, unsigned int line_number, char *arg)
+/**
+ * pall - Prints all values on the stack
+ * @stack: Pointer to the stack
+ * @line_number: Line number in the script
+ */
+void pall(stack_t **stack, unsigned int line_number)
 {
-	(void)line_number;
-	(void)arg;
 	stack_t *current = *stack;
-
-	while (current)
+	(void)line_number; /*Parametro no utilizado*/
+	while (current != NULL)
 	{
 		printf("%d\n", current->n);
 		current = current->next;
 	}
 }
 
-void monty(char *file_path)
+/**
+ * pint - Prints the first values on the stack
+ * @stack: Pointer to the stack
+ * @line_number: Line number in the script
+ */
+void pint(stack_t **stack, unsigned int line_number)
 {
-	FILE *file = fopen(file_path, "r");
-	if (!file)
-	{
-		printf("Error: No se puede abrir el archivo %s\n", file_path);
-		exit(EXIT_FAILURE);
-	}
+	stack_t *current = *stack;
+	(void)line_number; /*Parametro no utilizado*/
 
-	stack_t *stack = NULL; // Pila inicialmente vacía
-	char *input = NULL;
-	size_t input_len = 0;
-	unsigned int line_number = 0;
-
-	instructions instr[] = {
-		{"push", push},
-		{"pall", pall},
-		{NULL, NULL}
-	};
-
-	while (getline(&input, &input_len, file) != -1)
-        {
-		line_number++;
-                input[strcspn(input, "\n")] = '\0';
-
-                char opcode[256];
-                char arg[256];
-
-                if (sscanf(input, "%255s %255s", opcode, arg) == 2)
-                {
-			int found = 0;
-                        for (unsigned int j = 0; instr[j].opcode; j++)
-			{
-				if (strcmp(instr[j].opcode, opcode) == 0)
-                                {
-					instr[j].f(&stack, line_number, arg); // Ejecutar la operación correspondiente
-                                        found = 1;
-                                        break;
-                                }
-		        }
-                        if (!found)
-			{
-				printf("Error en la línea %u: instrucción desconocida '%s'\n", line_number, opcode);
-				exit(EXIT_FAILURE);
-                        }
-		}
-	}
-
-	fclose(file);
-
-	while (stack)
-	{
-		stack_t *temp = stack;
-		stack = stack->next;
-		free(temp);
-	}
-
-	free (input);
+	printf("%d\n", current->n);
 }
 
-int main(int argc, char *argv[])
+/**
+ * pop - removes the first values on the stack
+ * @stack: Pointer to the stack
+ * @line_number: Line number in the script
+ */
+void pop(stack_t **stack, unsigned int line_number)
 {
-	if (argc != 2)
+	stack_t *node_to_delete = *stack;
+	(void)line_number;/*Parametro no Utilizado*/
+
+	*stack = node_to_delete->next;
+	if (node_to_delete->next)
+		node_to_delete->next->prev = NULL;
+	free(node_to_delete);
+}
+
+/**
+ * free_dlistint - function that frees a list
+ * @head: pointer to the header of the nodes
+ * Return: void
+ */
+void free_dlistint(stack_t *stack)
+{
+	stack_t *tmp;
+	while (stack != NULL)
 	{
-		printf("USO: %s archivo\n", argv[1]);
-		return (EXIT_FAILURE);
+		tmp = stack;
+		stack = stack->next;
+		free(tmp);
 	}
-
-	char *file_path = argv[1];
-	monty(file_path);
-
-	return (0);
 }
