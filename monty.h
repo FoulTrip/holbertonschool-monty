d2316f556bb119251f62062351e6145fecc44fa6
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-09-07 23:31:10 -0500

    act007

--- monty.h
+++ monty.h
@@ -46,6 +46,8 @@ void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
 void free_dlistint(stack_t *stack);
 size_t dlistint_len(const stack_t *stack);
+
 #endif
